f9238702684ec30929b1b726bde834b9840cc078
IRWAN\irwan <irwansyahputra92@gmail.com>
2022-06-26 20:38:24 +0700

    Add File Limit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const HOST = '0.0.0.0'
 const app = express()
 const limiter = rateLimit({
     windowMs: 1, // 15 minutes
-    max: 6, // Limit each IP to 100 requests per `window` (here, per 1 minutes)
+    max: 6, // Limit each IP to 6 requests per `window` (here, per 1 minutes)
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 })
