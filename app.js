3357a1fd1ef88d9fae14011bb0f9e01610a17c66
IRWAN\irwan <irwansyahputra92@gmail.com>
2022-06-26 21:27:01 +0700

    Add File Limit

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ const rateLimit = require('express-rate-limit')
 const express = require('express')
 
 const PORT = 8000
-const HOST = '0.0.0.0'
+const HOST = '127.0.0.1'
 
 const app = express()
-const limiter = rateLimit({
-    windowMs: 1, // 15 minutes
-    max: 6, // Limit each IP to 6 requests per `window` (here, per 1 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+// Create the rate limit rule
+const RequestLimiter = rateLimit({
+    windowMs: 10 * 1000, // 10 Second *1000=mili
+    max: 1, // limit each IP to 1 requests per windowMs
+    handler: function (req, res, /*next*/) {
+        return res.status(429).json({
+            error: 'You sent too many requests. Please wait a while then try again'
+        })
+    }
 })
-// Apply the rate limiting middleware to API calls only
-app.use('/', limiter)
+
+// Use the limit rule as an application middleware
+app.use(RequestLimiter)
+
 app.get('/', (req, res) => {
     res.send('<h1>Submission Irwan Syahputra</h1>')
 })
